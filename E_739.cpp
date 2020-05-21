a597b7d4182da6f2e10d230df0b4e4b21ac6475d
FannyZhao123 <f25zhao@uwaterloo.ca>
2020-05-21 19:27:59 -0400

    Create E_739.cpp

---
+++ E_739.cpp
@@ -0,0 +1,27 @@
+/*
+739. Daily Temperatures
+Given a list of daily temperatures T, return a list such that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+
+For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+*/
+
+//Runtime: 252 ms, faster than 14.88% of C++ online submissions for Daily Temperatures.
+//Memory Usage: 37.5 MB, less than 8.00% of C++ online submissions for Daily Temperatures.
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        vector<int> result(T.size());
+        stack<int> st;
+        int s = T.size();
+        for (int i = 0; i<s; i++){
+            while (!st.empty() && T[st.top()] < T[i]){
+                result[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return result;
+    }
+};
